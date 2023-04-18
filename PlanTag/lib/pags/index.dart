8f26a2b085d16d577621a443c31c209fdd6dc358
Emma <e@gmail.com>
2023-04-18 19:13:53 +0200

    Calendario con los días de la semana bien y favicon

--- PlanTag/lib/pags/index.dart
+++ PlanTag/lib/pags/index.dart
@@ -17,11 +17,9 @@ class Index extends StatefulWidget {
   State<Index> createState() => _IndexState();
 }
 
-
 // ********************************************************** CLASE PRINCIPAL **********************************************
 
 class _IndexState extends State<Index> {
-
 // Creamos la propiedad para que el valor del dropdown item cambie cuando se selecciona
   String _orderSelected = "";
 
@@ -34,27 +32,50 @@ class _IndexState extends State<Index> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             // Le ponemos el título asignado arriba en el widget
-            Text(widget.widget.title, style: const TextStyle(fontSize: 20, color: Colors.white, fontFamily: 'Trajan Pro'),),
-            Text('App', style: const TextStyle(fontSize: 20, color: Colors.grey, fontFamily: 'Trajan Pro'),),
+            Text(
+              widget.widget.title,
+              style: const TextStyle(
+                  fontSize: 20, color: Colors.white, fontFamily: 'Trajan Pro'),
+            ),
+            Text(
+              'App',
+              style: const TextStyle(
+                  fontSize: 20, color: Colors.grey, fontFamily: 'Trajan Pro'),
+            ),
           ],
         ),
-        actions: [Row(children: [
-          DropdownButton(items: <String>["Por fecha", "Por creación"].map((i) => DropdownMenuItem<String>(value: i, child: Text(i, style: const TextStyle(color: Colors.deepOrange)))).toList(), 
-          // Le colocamos texto inicial al menú deslegable, si la propiedad orderselected es "" entonces le ponemos el txt de la const
-          hint: _orderSelected == "" ? const Text("Seleccionar", style: TextStyle(color: Colors.black),
+        actions: [
+          Row(
+            children: [
+              DropdownButton(
+                  items: <String>["Por fecha", "Por creación"]
+                      .map((i) => DropdownMenuItem<String>(
+                          value: i,
+                          child: Text(i,
+                              style:
+                                  const TextStyle(color: Colors.deepOrange))))
+                      .toList(),
+                  // Le colocamos texto inicial al menú deslegable, si la propiedad orderselected es "" entonces le ponemos el txt de la const
+                  hint: _orderSelected == ""
+                      ? const Text(
+                          "Seleccionar", style: TextStyle(color: Colors.black),
 
-          // Sino
-          ): Text(_orderSelected, style: const TextStyle(color: Colors.black)),
-          onChanged: (value){
-            setState(() {
-                _orderSelected = value.toString(); 
-            });
-          }),
+                          // Sino
+                        )
+                      : Text(_orderSelected,
+                          style: const TextStyle(color: Colors.black)),
+                  onChanged: (value) {
+                    setState(() {
+                      _orderSelected = value.toString();
+                    });
+                  }),
 
-          // ---------------------------------------- Colocamos un logo al lado del desplegable -------------------
-          const _LogoSettings(),
-        ],)], 
-       
+              // ---------------------------------------- Colocamos un logo al lado del desplegable -------------------
+              const _LogoSettings(),
+            ],
+          )
+        ],
+
         backgroundColor: const Color.fromARGB(255, 78, 241, 190),
         toolbarHeight: 60,
         // Le añadimos sombra a la parte de debajo de la barra
@@ -87,16 +108,17 @@ class _IndexState extends State<Index> {
               child: Container(
                 color: const Color.fromARGB(255, 185, 249, 243),
                 child: SfDateRangePicker(
+                  monthViewSettings:
+                      const DateRangePickerMonthViewSettings(firstDayOfWeek: 1),
                   // Seleccion por rango
                   startRangeSelectionColor: Colors.purple,
                   endRangeSelectionColor: Colors.purple,
                   rangeSelectionColor: Colors.yellow,
                   selectionColor: Colors.black,
-                  selectionMode:  DateRangePickerSelectionMode.range,
+                  selectionMode: DateRangePickerSelectionMode.range,
 
                   // Ponemos un rango por defecto
                   initialSelectedRange: PickerDateRange(
-
                     // Desde hace 3 días hasta después de 7 días
                     DateTime.now().subtract(const Duration(days: 3)),
                     DateTime.now().add(const Duration(days: 7)),
@@ -108,17 +130,17 @@ class _IndexState extends State<Index> {
                   cancelText: "Cancelar",
 
                   // Cuando aceptas el rango seleccionado
-                  onSubmit: (DateRange){
+                  onSubmit: (DateRange) {
                     print(DateRange);
                   },
 
                   // Cuando cambiamos la fecha seleccionada
-                  onSelectionChanged: (DateRange){
+                  onSelectionChanged: (DateRange) {
                     print(DateRange.value);
                   },
                 ),
               ),
-              )
+            )
           ],
         ),
       ),
@@ -133,88 +155,87 @@ class _IndexState extends State<Index> {
 
 // ********************************************************** CLASE LOGO **********************************************
 
-
-class _LogoSettings extends StatelessWidget{
-  const _LogoSettings ({Key? key}) : super (key:key);
+class _LogoSettings extends StatelessWidget {
+  const _LogoSettings({Key? key}) : super(key: key);
 
   @override
-  Widget build (BuildContext context){
-
+  Widget build(BuildContext context) {
     // Le ponemos que tenga detección de gestos para que le podamos hacer click y definimos el método onTap debajo:
     return GestureDetector(
       child: const Padding(
         padding: EdgeInsets.all(10.0),
-        child: CircleAvatar(backgroundImage: AssetImage("assets/images/logo.jpg")),
+        child:
+            CircleAvatar(backgroundImage: AssetImage("assets/images/logo.jpg")),
       ),
-      onTap: (){
+      onTap: () {
         showCupertinoDialog(
-          context: context, 
-          builder: (context) {
-          return _Dialogo();
-        } );
+            context: context,
+            builder: (context) {
+              return _Dialogo();
+            });
       },
     );
   }
-} 
+}
 
 // ********************************************************** CLASE DIALOGO **********************************************
 
-class _Dialogo extends StatelessWidget{
-
+class _Dialogo extends StatelessWidget {
   final _key = GlobalKey<FormState>();
 
   final _nomFld = TextEditingController();
   final _imgFld = TextEditingController();
-  _Dialogo ({Key? key}) : super (key:key);
+  _Dialogo({Key? key}) : super(key: key);
 
   @override
-  Widget build (BuildContext context){
-
+  Widget build(BuildContext context) {
     return AlertDialog(
       content: SizedBox(
-        // Sacamos la altura y anchura del padre o de la ventana principal en este caso y le decimos que queremos ocupar un 40% de la misma  
-        width: MediaQuery.of(context).size.width* .4,
-        height: MediaQuery.of(context).size.height* .4,
+        // Sacamos la altura y anchura del padre o de la ventana principal en este caso y le decimos que queremos ocupar un 40% de la misma
+        width: MediaQuery.of(context).size.width * .4,
+        height: MediaQuery.of(context).size.height * .4,
         child: Form(
           key: _key,
-          child: Column(children: [
+          child: Column(
+            children: [
+              // Logo
+              const _LogoSettings(),
 
-            // Logo  
-            const _LogoSettings(),
-
-            // TxtFld Nombre
-            TextFormField(
-              controller: _nomFld,
-              decoration: const InputDecoration(labelText: "Nombre", border: OutlineInputBorder()),
-            ),
-
-            // Le añadimos un espacio para que no estén tan pegados como si fuera un br
-            const SizedBox(height: 10),
-
-             // TxtFld img
-            TextFormField(
-              controller: _imgFld,
-              decoration: const InputDecoration(labelText: "Nombre", border: OutlineInputBorder()),
-            ),
-
-            // Le añadimos un espacio para que no estén tan pegados como si fuera un br
-            const SizedBox(height: 15),
-
-            TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.amber,
-                // Color del texto
-                foregroundColor: Colors.black,
-                elevation: 4
+              // TxtFld Nombre
+              TextFormField(
+                controller: _nomFld,
+                decoration: const InputDecoration(
+                    labelText: "Nombre", border: OutlineInputBorder()),
               ),
-              onPressed: (){
-              print("object");
-            },
-            //Boton y txt del boton
-            child: const Text("Plantar")),
-          ],),
+
+              // Le añadimos un espacio para que no estén tan pegados como si fuera un br
+              const SizedBox(height: 10),
+
+              // TxtFld img
+              TextFormField(
+                controller: _imgFld,
+                decoration: const InputDecoration(
+                    labelText: "Nombre", border: OutlineInputBorder()),
+              ),
+
+              // Le añadimos un espacio para que no estén tan pegados como si fuera un br
+              const SizedBox(height: 15),
+
+              TextButton(
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.amber,
+                      // Color del texto
+                      foregroundColor: Colors.black,
+                      elevation: 4),
+                  onPressed: () {
+                    print("object");
+                  },
+                  //Boton y txt del boton
+                  child: const Text("Plantar")),
+            ],
+          ),
         ),
       ),
     );
   }
-} 
+}
