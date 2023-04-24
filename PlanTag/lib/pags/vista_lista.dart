bed697d38ca100d1ce702d28bc3898fbc488f0f7
Irune <iruneguinea29@gmail.com>
2023-04-24 18:00:29 +0200

    fix

--- PlanTag/lib/pags/vista_lista.dart
+++ PlanTag/lib/pags/vista_lista.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:plantag/models/tarea.dart';
 import 'package:plantag/database_helper.dart';
@@ -55,6 +58,7 @@ class VistaLista extends StatelessWidget {
       // ---------------------- Boton para añadir tareas --------------------//
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+            print("no llega");
             Tarea tarea = Tarea(
               id: 1,
               titulo: "Comprar comida",
@@ -67,7 +71,6 @@ class VistaLista extends StatelessWidget {
               prioridad: 2,
             );
         SQLHelper.insertarTarea(tarea);
-
         },
         backgroundColor: Colors.deepPurple,
         child: const Icon(Icons.add_task)
