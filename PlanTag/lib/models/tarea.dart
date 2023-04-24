90d6af5ed1216166b0f1189158745e685439fdeb
Irune <iruneguinea29@gmail.com>
2023-04-24 17:13:52 +0200

    modelo de tarea

---
+++ PlanTag/lib/models/tarea.dart
@@ -0,0 +1,28 @@
+
+class Tarea {
+  int? id;
+  String? titulo;
+  String? descripcion; 
+  DateTime? fechaInicio; 
+  DateTime? fechaFin;
+  String? categoria;
+  int? dificultad;
+  String? imagen;
+  int? prioridad;
+
+Tarea({this.id, this.titulo, this.descripcion, this.fechaInicio, this.fechaFin, this.categoria, this.dificultad, this.imagen, this.prioridad});
+
+Map<String, dynamic> toMap() {
+return {
+'id': id,
+'titulo': titulo,
+'descripcion': descripcion,
+'fechaInicio': fechaInicio.toString(),
+'fechaFin': fechaFin.toString(),
+'categoria': categoria,
+'dificultad': dificultad,
+'imagen': imagen,
+'prioridad': prioridad
+};
+}
+}
