a27bb6936af9b79cc2c2b6b7920f2f305a5310c9
Irune <iruneguinea29@gmail.com>
2023-05-19 11:12:36 +0200

    categorias recarga bien

--- PlanTag/lib/pags/categorias.dart
+++ PlanTag/lib/pags/categorias.dart
@@ -24,57 +24,73 @@ class _CategoriasPageState extends State<CategoriasPage> {
     });
   }
 
+ void _mostrarDialogoCategoria() async {
+  final result = await showDialog(
+    context: context,
+    builder: (BuildContext context) => DialogoCategoria(
+      onClose: (result) {
+        // Handle the result returned from the dialog here
+        if (result != null) {
+          _fetchCategorias();
+        }
+      },
+    ),
+  );
 
-
-  void _mostrarDialogoCategoria() {
-     showDialog(
-                  context: context,
-                  builder: (BuildContext context) => DialogoCategoria(
-                  
-                  ),
-    );
+  // Handle the result returned from the dialog here
+  if (result != null) {
+    _fetchCategorias();
   }
+}
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-           appBar: AppBar(
-              title: Text(
-                "Categorias",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontFamily: 'Trajan Pro',
-                ),
-              ),
-        backgroundColor: Color.fromRGBO(163, 238, 176, 1),
-        toolbarHeight: 60,
-        elevation: 5,
-      ),
-      body: ListView.builder(
-        itemCount: _categorias.length,
-        itemBuilder: (context, index) {
-          final categoria = _categorias[index];
-          return ListTile(
-            leading: IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () => {
-                SQLHelper.eliminarCategoria(categoria),
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>  CategoriasPage()),
-                  )
-              }
+    return WillPopScope(
+      onWillPop: () async {
+        // Custom logic to handle back button press
+        // Return false to prevent leaving the page via the back button
+
+        // Example: Go back to the previous page
+        Navigator.pop(context);
+
+        // Return false to prevent leaving the page via the back button
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Categorias",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontFamily: 'Trajan Pro',
             ),
-            title: Text(categoria),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _mostrarDialogoCategoria,
-        backgroundColor: Colors.deepPurple,
-        child: Icon(Icons.add),
+          ),
+          backgroundColor: Color.fromRGBO(163, 238, 176, 1),
+          toolbarHeight: 60,
+          elevation: 5,
+        ),
+        body: ListView.builder(
+          itemCount: _categorias.length,
+          itemBuilder: (context, index) {
+            final categoria = _categorias[index];
+            return ListTile(
+              leading: IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () async {
+                  await SQLHelper.eliminarCategoria(categoria);
+                  _fetchCategorias();
+                },
+              ),
+              title: Text(categoria),
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _mostrarDialogoCategoria,
+          backgroundColor: Colors.deepPurple,
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
--- PlanTag/lib/widgets/dialogo.dart
+++ PlanTag/lib/widgets/dialogo.dart
@@ -1,150 +1,126 @@
-// ********************************************************** CLASE DIALOGO **********************************************
 import 'package:flutter/material.dart';
 import 'package:plantag/database_helper.dart';
-import 'package:plantag/main.dart';
+import 'package:plantag/pags/categorias.dart';
 import 'package:plantag/widgets/logo.dart';
-
 
 class DialogoCategoria extends StatelessWidget {
   final _key = GlobalKey<FormState>();
-  // Para saber siempre el tamaño de la columna del formulario de forma dinámica aunque vaya cambiando por la panatalla
   final _keyTamColum = GlobalKey<FormState>();
-
   final _nomFld = TextEditingController();
-  DialogoCategoria({Key? key}) : super(key: key);
+  final Function(dynamic) onClose;
+
+  DialogoCategoria({Key? key, required this.onClose}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text(
-        "Nueva categoría",
-        textAlign: TextAlign.left,
-      ),
-      backgroundColor: Color.fromARGB(255, 223, 255, 222), // your color
-      shape: RoundedRectangleBorder(
-          borderRadius:
-              BorderRadius.circular(40)), // change 40 to your desired radius
-      // ------------------------------------------- CONSTRAINT LAYOUT  -------------------------------------------
-      // Dependiendo de la pantalla, especificamos los tamaños estandar
-      content: ConstrainedBox(
-        constraints: const BoxConstraints(
-          minWidth: 100,
-          maxWidth: 1000,
-          minHeight: 200,
-          maxHeight: 200,
+    return WillPopScope(
+      onWillPop: () async {
+        // Custom logic to handle back button press within the dialog
+        // Return false to prevent the dialog from being closed by the back button
+
+        // Example: Close the dialog and navigate to the previous page
+        Navigator.of(context).pop();
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => CategoriasPage()),
+        );
+
+        // Return false to prevent the dialog from being closed by the back button
+        return false;
+      },
+      child: AlertDialog(
+        title: const Text(
+          "Nueva categoría",
+          textAlign: TextAlign.left,
         ),
-        child: SizedBox(
-          // Sacamos la altura y anchura del padre o de la ventana principal en este caso y le decimos que queremos ocupar un 40% de la misma
-          width: MediaQuery.of(context).size.width * .4,
-          height: MediaQuery.of(context).size.height * .4,
-
-          // ----------------------------------------- FORMULARIO DEL DIALOGO ------------------------------------
-
-          child: Form(
-            // Le decimos a flutter que efectivamente le asignamos esta key al componente Form para poderlo evaluar abajo en el método de presionar el botón
-            key: _key,
-            // Para que sea más responsive, si no entra en la pantalla le metemos un scroll view al diálogo que solo aparece cuando sea necesario de forma automática
-            child: SingleChildScrollView(
-              child: Column(
-                // Para saber el tamaño de un componente en particular solamente le estableces su key y lo evaluamos al presionar el botón por ejemplo
-                key: _keyTamColum,
-                children: [
-                  // ------------------------------------------- Logo --------------------------------------------
-                  const LogoSettings(),
-
-                  // -------------------------------------- TxtFld Nombre ----------------------------------------
-                  TextFormField(
-                    controller: _nomFld,
-                    decoration: const InputDecoration(
-                        labelText: "Nombre", border: OutlineInputBorder()),
-
-                    // Validamos si es correcto ese campo o si está relleno en caso de ser obligatorio (Cuando se pulsa el botón abajo se valida)
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor introduce algo';
-                      }
-                      return null;
-                    },
-                  ),
-
-                  // Le añadimos un espacio para que no estén tan pegados como si fuera un br
-                  const SizedBox(height: 10),
-
-                 
-
-                  // -------------------------------------- Btns Formulario ----------------------------------------
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // ---------------------------------------- Btn Cancel --------------------------------------
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 18.0, left: 2),
-                        child:
-                      OutlinedButton.icon(
-                        icon: const Icon(Icons.close),
-                        label: const Text("Cancelar"),
-                        onPressed: () {
-                          // Cerramos la ventana cuando pulsemos el botón
-                          Navigator.pop(context);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 10),
-                          side: const BorderSide(
-                              width: 2.0,
-                              color: Color.fromARGB(255, 255, 9, 9)),
-                          foregroundColor: const Color.fromARGB(255, 255, 9, 9),
-                          // Forma de cuadrado circular al botón
-                          shape: const StadiumBorder(),
-                        ),
+        backgroundColor: Color.fromARGB(255, 223, 255, 222),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+        content: ConstrainedBox(
+          constraints: const BoxConstraints(
+            minWidth: 100,
+            maxWidth: 1000,
+            minHeight: 200,
+            maxHeight: 200,
+          ),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * .4,
+            height: MediaQuery.of(context).size.height * .4,
+            child: Form(
+              key: _key,
+              child: SingleChildScrollView(
+                child: Column(
+                  key: _keyTamColum,
+                  children: [
+                    const LogoSettings(),
+                    TextFormField(
+                      controller: _nomFld,
+                      decoration: const InputDecoration(
+                        labelText: "Nombre",
+                        border: OutlineInputBorder(),
                       ),
-                      ),
-
-                      // Le añadimos un espacio para que no estén tan pegados como si fuera un br
-                      const SizedBox(width: 10),
-
-                      // ---------------------------------------- Btn Ok --------------------------------------
-                     Padding(
-                      padding: const EdgeInsets.only(bottom: 18.0, right: 10),
-                      child:  OutlinedButton.icon(
-                        icon: const Icon(Icons.done),
-                        label: const Text("Aceptar"),
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 10),
-                          // Forma de cuadrado circular al botón
-                          shape: const StadiumBorder(),
-                          side: const BorderSide(
-                              width: 2.0,
-                              color: Color.fromARGB(255, 3, 223, 25)),
-                          //backgroundColor: Color.fromARGB(255, 94, 255, 110),
-                          // Color del texto
-                          foregroundColor: const Color.fromARGB(255, 5, 177, 22),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor introduce algo';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 18.0, left: 2),
+                          child: OutlinedButton.icon(
+                            icon: const Icon(Icons.close),
+                            label: const Text("Cancelar"),
+                            onPressed: () {
+                              Navigator.pop(context);
+                              onClose(null); // Pass null to the onClose callback
+                            },
+                            style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 10),
+                              side: const BorderSide(
+                                  width: 2.0,
+                                  color: Color.fromARGB(255, 255, 9, 9)),
+                              foregroundColor:
+                                  const Color.fromARGB(255, 255, 9, 9),
+                              shape: const StadiumBorder(),
+                            ),
+                          ),
                         ),
-                        // --------------------------------- Metodo pulsar btn --------------------------------------
-                        onPressed: () {
-                          // Depende de si está definido el contexto o no y de si está o no asignada la key a este componente por lo que arriba lo evaluamos con una condicion
-                          // y ponemos la ! para decirle a flutter que nos aseguramos que recibe esa variable
-
-                          print(_key.currentContext!.size);
-                          // Validamos si es correcto el formulario y están todos los campos rellenos o falta alguno obligatorio
-                          if (_key.currentState!.validate()) {
-                            // Process data.
-                            // Cerramos la ventana cuando pulsemos el botón
-
-                            SQLHelper.insertarCategoria(_nomFld.text);
-                           Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const MyApp()),
-                          );
-                          }
-                        },
-                      ),),
-                    ],
-                  ),
-                ],
+                        const SizedBox(width: 10),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 18.0, right: 10),
+                          child: OutlinedButton.icon(
+                            icon: const Icon(Icons.done),
+                            label: const Text("Aceptar"),
+                            style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 10),
+                              shape: const StadiumBorder(),
+                              side: const BorderSide(
+                                  width: 2.0,
+                                  color: Color.fromARGB(255, 3, 223, 25)),
+                              foregroundColor:
+                                  const Color.fromARGB(255, 5, 177, 22),
+                            ),
+                            onPressed: () {
+                              if (_key.currentState!.validate()) {
+                                SQLHelper.insertarCategoria(_nomFld.text);
+                                Navigator.pop(context);
+                                onClose(true); // Pass the result to the onClose callback
+                              }
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
